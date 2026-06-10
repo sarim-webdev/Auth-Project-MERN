bb0b14c8bc98b91271fadbba07e7d774e3dcdda4
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-06-10 16:32:21 +0500

    Auth-System-MERN

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4500/api",
+  baseURL: "https://auth-project-mern-r5wa.vercel.app/api",
   withCredentials: true,
 });
 
